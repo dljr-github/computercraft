f53aa2183772aca7cd6d4452cb1f22abe63284fa
helpmyRF24isntworking <52329548+helpmyRF24isntworking@users.noreply.github.com>
2025-03-06 20:35:32 +0100

    Add files via upload

--- pocket/startup.lua
+++ pocket/startup.lua
@@ -1,20 +1,30 @@
 
+os.loadAPI("/runtime/bluenet.lua")
 shell.run("runtime/update.lua")
 
+-- add runtime as default environment
+package.path = package.path ..";../runtime/?.lua"
+--package.path = package.path .. ";../?.lua" .. ";../runtime/?.lua"
+--require("classMonitor")
+--require("../runtime/classMonitor")
+--require("runtime/classMonitor")
+
+if rednet then	
+	shell.run("runtime/killRednet.lua")
+	return
+end
+
 os.loadAPI("/runtime/global.lua")
+os.loadAPI("/runtime/config.lua")
 
 shell.run("runtime/initialize.lua")
-tabMain = shell.openTab("runtime/main.lua")
-tabReceive = shell.openTab("runtime/receive.lua")
-tabSend = shell.openTab("runtime/send.lua")
 
+shell.openTab("runtime/display.lua")
+shell.openTab("runtime/main.lua")
+shell.openTab("runtime/receive.lua")
+shell.openTab("runtime/send.lua")
+--shell.openTab("runtime/send.lua")
+--shell.openTab("runtime/update.lua")
 
-multishell.setTitle(tabMain, "main")
-multishell.setTitle(tabReceive, "receive")
-multishell.setTitle(tabSend, "send")
+--shell.run("runtime/testMonitor")
 
--- MultiShell tests: 
--- shell.openTab("/multi/multi_1.lua")
--- shell.openTab("multi/multi_2")
-
---shell.openTab better than multishell.launch because it does not disable some global functionalities like require()
--- pocket/update.lua
+++ pocket/update.lua
@@ -1,6 +1,8 @@
-require("classNetworkNode")
+require("classBluenetNode")
+--require("classNetworkNode")
 
-node = NetworkNode:new("update")
+local node = NetworkNode:new("update")
+local waitTime = 5
 
 folders = {
 "general",
@@ -12,44 +14,84 @@ folders = {
 files = {
 --"classList.lua",
 --"classNetworkNode.lua",
-}
+}  
+ 
+local restart = false 
+ 
+-- local function getFileAttributes(file)
+	-- if fs.exists(file) then 
+		-- local modified = fs.attributes(file).modified
+		-- return { fileName = file, modified = modified
+-- end
+local function getFileAttributes(folder)
+	local fileAttributes = {}
+	if fs.exists(folder) and fs.isDir(folder) then 
+		
+		for _, fileName in ipairs(fs.list("/"..folder)) do 
+			local modified = fs.attributes(folder .."/"..fileName).modified
+			fileAttributes[fileName] = { modified = modified }
+		end
+	end
+	return fileAttributes
+end 
 
-local restart = false
-
-local function importFile(fileName, fileData)
-	file = fs.open(fileName, "r")
-	local currentData
+local function compareFiles(f1, f2)
+	local file = fs.open(f1, "r")
+	local data1, data2
 	if file then
-		currentData = file.readAll()
+		data1 = file.readAll()
 		file.close()
 	end
-	if (currentData == fileData) == false then
-		print("updating", fileName)
-		restart = true
-		file = fs.open(fileName, "w")
-		file.write(fileData)
+	local file = fs.open(f2, "r")
+	if file then 
+		data2 = file.readAll()
 		file.close()
 	end
+	if data1 and data2 then
+		return data1 == data2
+	end
+	return false
+end
+local function importFile(fileName, fileData)
+
+	print("updating", fileName)
+	restart = true
+	if fs.exists(fileName) then 
+		fs.delete(fileName)
+	end
+	file = fs.open(fileName, "w")
+	file.write(fileData)
+	file.close()
+	
 end
 
-node.onReceive = function(msg)
+node.onNoAnswer = function(forMsg)
+	print("NO ANSWER",forMsg.data[1])
+end 
+
+function handleAnswer(msg,forMsg)
 	if msg and msg.data then
+		
 		if msg.data[1] == "FILE" then
-			print("received",msg.data[2].fileName)
-			importFile(msg.data[2].fileName, msg.data[2].fileData)			
-		elseif msg.data[1] == "FOLDER" then
-			for _,file in ipairs(msg.data[2]) do
-				if file.fileName == "startup.lua" then
-					importFile(file.fileName, file.fileData)
-				else
-					importFile("runtime/"..file.fileName, file.fileData)
+			print("received",msg.data[2].name)
+			importFile(msg.data[2].name, msg.data[2].data)			
+		elseif msg.data[1] == "FOLDERS" then
+			
+			for folderName, folder in pairs(msg.data[2]) do
+				for fileName,file in pairs(folder) do
+					print("importing", folderName, fileName)
+					if fileName == "startup.lua" then
+						importFile(fileName, file.data)
+					else
+						importFile("runtime/"..fileName, file.data)
+					end
 				end
 			end
 		else
-			print(msg.data[1], msg.data[2].fileName)
+			print(msg.data[1], textutils.serialize(msg.data[2]))
 		end
 	end
-end
+end 
 
 if not node.host then
 	print("NO UPDATE HOST AVAILABLE")
@@ -57,17 +99,83 @@ else
 	print("updating...")
 	for _,file in ipairs(files) do
 		print("requesting", file)
-		local data = { "FILE_REQUEST", { fileName = file } }
-		node:send(node.host, data, false)
-		node:listen(1)
+		local modified = nil
+		if fs.exists(file) then 
+			modified = fs.attributes(file).modified
+		end
+		local data = { "FILE_REQUEST", { fileName = file, modified = modified } }
+		local answer, forMsg = node:send(node.host, data, true, true, waitTime)
+		handleAnswer(answer, forMsg)
 	end
-	for _,folder in ipairs(folders) do
-		print("requesting folder", folder)
-		local data = { "FOLDER_REQUEST", { folderName = folder } }
-		node:send(node.host, data, false)
-		node:listen(1) -- listen for max 1 second
+	local files = getFileAttributes("/runtime")
+	if fs.exists("startup.lua") then 
+		files["startup.lua"] = { modified = fs.attributes("startup.lua").modified }
 	end
+	
+	print("requesting folders")
+	local data = { "FOLDERS_REQUEST", { folderNames = folders, files = files } }
+	local answer, forMsg = node:send(node.host, data, true, true, waitTime)
+	handleAnswer(answer, forMsg)
+
 	if restart then
 		os.reboot()
 	end
 end
+
+
+-- raw file request.. basically just bluenet, dont like it very much.
+
+-- local modem
+-- local waitTime = 5
+-- local protocol = "update"
+-- local host
+
+-- local function waitForAnswer(protocol, waitTime)
+	-- -- identical to bluenet
+	
+	-- local timer = nil
+	-- local eventFilter = nil
+	
+	-- if waitTime then
+		-- timer = os.startTimer(waitTime)
+		-- eventFilter = nil
+	-- else
+		-- eventFilter = "modem_message"
+	-- end
+	
+	-- --print("receiving", protocol, waitTime, timer, eventFilter)
+	-- while true do
+		-- local event, modem, channel, sender, msg, distance = os.pullEvent(eventFilter)
+		
+		-- if event == "modem_message" 
+			-- and type(msg) == "table" 
+			-- and ( type(msg.recipient) == "number" and msg.recipient
+			-- and ( msg.recipient == computerId or msg.recipient == default.channels.broadcast 
+				-- or msg.recipient == default.channels.host ) )
+			-- and ( protocol == nil or protocol == msg.protocol )
+			-- -- just to make sure its a bluenet message
+			-- then
+				-- msg.distance = distance
+				-- return msg
+				
+		-- elseif event == "timer" then
+			-- if modem == timer then
+				-- return nil
+			-- end
+		-- end
+		
+	-- end
+
+-- end
+
+-- local function requestFiles()
+	-- modem = peripheral.find("modem")
+	-- if not modem then 
+		-- print("NO MODEM")
+		-- return nil
+	-- end
+	-- peripheral.call(modem, "open", os.getComputerID()%65400)
+	
+
+-- end
+
